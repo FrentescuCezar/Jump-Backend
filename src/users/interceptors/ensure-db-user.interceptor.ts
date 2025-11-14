416419d41edb89d5913c6d8b6600dc6fd6ce9c8b
Frentescu Cezar-Augustin <cezar.frentescu@rinf.tech>
2025-11-15 00:19:48 +0200

    feat(Users): Ensure Keycloak-authenticated users are persisted locally using interceptors

---
+++ src/users/interceptors/ensure-db-user.interceptor.ts
@@ -0,0 +1,35 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from "@nestjs/common"
+import type { Observable } from "rxjs"
+import type { Request } from "express"
+import type { User } from "@prisma/client"
+import type { AuthenticatedUser } from "../../common/types/authenticated-user"
+import { UsersService } from "../users.service"
+
+type RequestWithUser = Request & {
+  user?: AuthenticatedUser
+  dbUser?: User
+}
+
+@Injectable()
+export class EnsureDbUserInterceptor implements NestInterceptor {
+  constructor(private readonly usersService: UsersService) {}
+
+  async intercept(
+    context: ExecutionContext,
+    next: CallHandler,
+  ): Promise<Observable<unknown>> {
+    const request = context.switchToHttp().getRequest<RequestWithUser>()
+    const authUser = request?.user
+
+    if (authUser?.sub) {
+      request.dbUser = await this.usersService.ensureUserEntity(authUser)
+    }
+
+    return next.handle()
+  }
+}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,10 +1,19 @@
 import { Module } from "@nestjs/common"
+import { APP_INTERCEPTOR } from "@nestjs/core"
 import { UsersController } from "./users.controller"
 import { UsersService } from "./users.service"
+import { EnsureDbUserInterceptor } from "./interceptors/ensure-db-user.interceptor"
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [
+    UsersService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: EnsureDbUserInterceptor,
+    },
+  ],
+  exports: [UsersService],
 })
 export class UsersModule {}
 
